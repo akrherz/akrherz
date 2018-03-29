d4eec323939811715e91105517abd42a6b0db592
akrherz <akrherz@iastate.edu>
2018-03-29 14:54:01 -0500

    sync presentation content

---
+++ presentations/061205_polygon/extract.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import glob, re
+
+files = glob.glob('output/svr_2005_???.html')
+for file in files:
+  wfo = re.findall("([A-Z]{3}).html", file)[0]
+  d = open(file, 'r').read()
+  far, pod, csi = re.findall("PARSEME: FAR:([0-9\.]*) POD:([0-9\.]*) CSI:([0-9\.]*)", d)[0]
+  print "%s,%s,%s,%s," % (wfo, far, pod, csi)
