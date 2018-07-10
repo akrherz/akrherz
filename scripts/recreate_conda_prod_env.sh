c6e8461e648ae334a2a5598f04e1423ec87b07b4
akrherz <akrherz@iastate.edu>
2018-07-10 07:13:36 -0500

    create conda prod env on demand

---
+++ scripts/recreate_conda_prod_env.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# `conda update --all` is not working well with conda-forge and I want
+# to use conda-forge, so we need to be able to reproduce the conda env
+# on demand, maybe life will be better this way
+. /opt/miniconda3/etc/profile.d/conda.sh
+set -x
+
+# switch back to root env, so we can delete the prod env
+conda activate root
+# save a backup
+conda create --name prod.$(date -u +%Y%m%d%H%M%S) --clone prod
+# delete, gasp
+conda remove -n prod --all -y
+# create the env
+conda create -n prod python=3.6 pip -y
+# switch to it
+conda activate prod
+
+# do project needs
+for project in iem iembot pyIEM weather.im dep
+do
+  conda install -y --file /home/akrherz/projects/$project/conda_requirements.txt
+  pip install --upgrade -r /home/akrherz/projects/$project/pip_requirements.txt
+done
+
+# extra stuff
+conda install -y flake8 pep8
+
+# manual stuff to do before uploading to servers
+# echo " + add CDATA patch to feedgen"
+echo " + need MANUAL edit of cartopy for 3 point polygon issue"
