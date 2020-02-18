8760eaafeb22ad348e812f744abdc2dedda0452b
akrherz <akrherz@iastate.edu>
2020-02-18 09:20:55 -0600

    WIP

--- presentations/190320_pub/coolness.py
+++ presentations/190320_pub/coolness.py
@@ -11,13 +11,13 @@ def main():
     for sid in nt.sts:
         data[sid if len(sid) == 3 else sid[1:]] = 5
 
-    data['PUB'] = 10
-    data['BOU'] = 2
+    data['DMX'] = 10
+    data['DVN'] = 2
 
     mp = MapPlot(sector='conus', axisbg='white',
                  title='Weather Bureau Office Coolness Factor')
     mp.fill_cwas(data, bins=range(0, 11), lblformat='%.0f', units='awesome',
-                 ilabel=True)
+                 ilabel=True, extend='neither')
     mp.postprocess(filename='coolness.png')
 
 
