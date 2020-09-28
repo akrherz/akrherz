00c32d6d04badf9a1f258d94bae16c2e6bc7aab4
akrherz <akrherz@iastate.edu>
2020-09-28 16:53:56 -0500

    finish EWX presentation

--- presentations/200825_ewx/README.md
+++
@@ -1,2 +0,0 @@
-
-[Google Doc with questions](https://docs.google.com/document/d/11LwogRjDuP7E1PRpVBilAPakGOG7vtNmiw07l0m-Sa8/edit)
---
+++ presentations/200929_ewx/coolness.py
@@ -0,0 +1,27 @@
+"""Simple"""
+from pyiem.network import Table as NetworkTable
+from pyiem.plot import MapPlot
+
+
+def main():
+    """GO"""
+    nt = NetworkTable("WFO")
+
+    data = {}
+    for sid in nt.sts:
+        data[sid if len(sid) == 3 else sid[1:]] = 5
+
+    data['EWX'] = 9
+    data['FWD'] = 1
+    data['SJT'] = 3
+    data['HGX'] = 6
+
+    mp = MapPlot(sector='state', state='TX', axisbg='white',
+                 title='Weather Bureau Office Coolness Factor')
+    mp.fill_cwas(data, bins=range(0, 11), lblformat='%.0f', units='awesome',
+                 ilabel=True, extend='neither')
+    mp.postprocess(filename='coolness.png')
+
+
+if __name__ == '__main__':
+    main()
