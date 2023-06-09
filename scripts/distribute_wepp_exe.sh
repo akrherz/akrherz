4ceab6990a0e71079d1a212a2e8a7fd19e641b92
akrherz <akrherz@iastate.edu>
2023-06-09 07:21:42 -0500

    distribute to DC nodes

--- scripts/distribute_wepp_exe.sh
+++ scripts/distribute_wepp_exe.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-for name in metvm4-dc iemvs101 metvm6-dc arritt iemvs102 iem8-dc metvm2-dc metvm33-dc; do
+for name in metvm4-dc iemvs35-dc iemvs36-dc metvm6-dc arritt iem8-dc metvm2-dc metvm33-dc; do
 	scp $1 mesonet@$name:bin/
 done
