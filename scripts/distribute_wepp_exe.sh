d25d58fb6e4d0349624a441f8d25991498fc800f
akrherz <akrherz@iastate.edu>
2024-04-12 16:06:07 -0500

    add more dep hosts

--- scripts/distribute_wepp_exe.sh
+++ scripts/distribute_wepp_exe.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-for name in metvm4-dc iemvs35-dc iemvs36-dc metvm6-dc arritt iem8-dc metvm2-dc metvm33-dc; do
+for name in metvm4-dc iemvs35-dc iemvs36-dc metvm6-dc arritt iem8-dc metvm2-dc metvm33-dc iemvm1 iemvm2 iemvm9; do
 	scp $1 mesonet@$name:bin/
 done
